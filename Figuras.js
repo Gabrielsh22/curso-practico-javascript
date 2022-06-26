cd8c06d86f06f2444a7c2c24b620951f7b4ba3a8
Gabrielsh22 <sebas.hernandez2206@gmail.com>
2022-06-26 15:12:02 -0500

    ACTUALIZACIONES

--- Figuras.js
+++ Figuras.js
@@ -53,7 +53,60 @@ const PI = Math.PI;
     return (radio * radio) * PI
  } 
 
+// Interacción con HTML:
 
+//Cuadrado
+function calcularPerimetroCuadrado(){
+    const input = document.getElementById("InputCuadrado");
+    const valor = input.value;
+    const perimetro = PerimetroCuadrado(valor);
+    alert(perimetro);
+}
 
+function calcularAreaCuadrado(){
+    const input = document.getElementById("InputCuadrado");
+    const valor = input.value;
+    const area = AreaCuadrado(valor);
+    alert(area);
+}
+//Triangulo
+function calcularPerimetroTriangulo(){
+    const input = document.getElementById("InputTriangulolado1");
+    const valor = Number(input.value);
+    const input2 = document.getElementById("InputTriangulolado2");
+    const valor2 = Number(input2.value);
+    const input3 = document.getElementById("InputTrianguloBase");
+    const valor3 = Number(input3.value);
+    const perimetro = PerimetroTriangulo(valor, valor2, valor3);
+    alert(perimetro);
+}
+function calcularAreaTriangulo(){
+    const input = document.getElementById("InputTrianguloBase");
+    const valor = Number(input.value);
+    const input2 = document.getElementById("InputTrinaguloAltura");
+    const valor2 = Number(input2.value);
+    const area = AreaTriangulo(valor, valor2);
+    alert(area);
+}
 
+//Circunferencia
+function calcularDiámetroCirculo(){
+    const input = document.getElementById("InputCirculoRadio");
+    const valor = input.value;
+    const diametro = diametroCircunferencia(valor);
+    alert(diametro);
+}
 
+function calcularPerimetroCirculo(){
+    const input = document.getElementById("InputCirculoRadio");
+    const valor = input.value;
+    const perimetro = perimetroCirculo(valor);
+    alert(perimetro);
+}
+
+function calcularAreaCirculo(){
+    const input = document.getElementById("InputCirculoRadio");
+    const valor = input.value;
+    const area = áreaCircunferencia(valor);
+    alert(area);
+}
